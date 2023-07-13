d4ee7438bc2e10a83c06dd11a36bdb586862b254
parthpawa1806 <parthpawa1806@gmail.com>
2023-07-13 16:15:37 +0530

    commit

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,13 +1,12 @@
 import 'dart:convert';
 import 'dart:io';
+
 import 'package:ecommerce_image_search/screens/result_page.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:http/http.dart' as http;
 import 'package:fluttertoast/fluttertoast.dart';
-
-import 'animation.dart';
-
+import 'package:image_cropper/image_cropper.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -15,7 +14,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  File? _image;
+  XFile? _image;
 
   Future<void> _pickImage(ImageSource source) async {
     final picker = ImagePicker();
@@ -23,16 +22,57 @@ class _HomeScreenState extends State<HomeScreen> {
 
     setState(() {
       if (pickedImage != null) {
-        _image = File(pickedImage.path);
+        _image = XFile(pickedImage.path);
+        _cropImage();
       } else {
         Fluttertoast.showToast(msg: 'No image selected');
       }
     });
   }
 
+  Future<void> _cropImage() async {
+    if (_image != null) {
+      final croppedFile = await ImageCropper().cropImage(
+        sourcePath: _image!.path,
+        compressFormat: ImageCompressFormat.jpg,
+        compressQuality: 100,
+        uiSettings: [
+          AndroidUiSettings(
+              toolbarTitle: 'Cropper',
+              toolbarColor: Colors.white,
+              toolbarWidgetColor: Colors.green,
+              initAspectRatio: CropAspectRatioPreset.original,
+              lockAspectRatio: false),
+          IOSUiSettings(
+            title: 'Cropper',
+          ),
+          WebUiSettings(
+            context: context,
+            presentStyle: CropperPresentStyle.dialog,
+            boundary: const CroppieBoundary(
+              width: 520,
+              height: 520,
+            ),
+            viewPort:
+                const CroppieViewPort(width: 480, height: 480, type: 'circle'),
+            enableExif: true,
+            enableZoom: true,
+            showZoomer: true,
+          ),
+        ],
+      ) ;
+      if (croppedFile != null) {
+        setState(() {
+        
+        });
+      }
+    }
+  }
+
+
   Future<void> _navigateToResultPage() async {
     if (_image != null) {
-      final url = 'http://192.168.42.251:8000/api/similar-images/'; 
+      final url = 'http://110.227.255.212:6021/api/similar-images/';
 
       var request = http.MultipartRequest('POST', Uri.parse(url));
       request.files.add(
@@ -66,15 +106,34 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        leading: Padding(
+          padding: const EdgeInsets.only(right: 20),
+          child: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.grey[500]),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+        ),
+        backgroundColor: Colors.white,
         elevation: 0,
-        title: Center(child: Text('Image Picker Demo',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.purple),)),
+        title: Center(
+          child: Padding(
+            padding: const EdgeInsets.only(right: 50),
+            child: Text(
+              'Visual Search',
+              style: TextStyle(
+                fontWeight: FontWeight.w100,
+                color: Colors.grey,
+                fontSize: 17,
+              ),
+            ),
+          ),
+        ),
       ),
       body: Stack(
         fit: StackFit.expand,
         children: [
           Image.asset(
-            'lib/assets/downlaaad.jpg',
+            'lib/assets/bgimageee.jpg',
             fit: BoxFit.cover,
           ),
           Container(
@@ -84,18 +143,37 @@ class _HomeScreenState extends State<HomeScreen> {
                   Colors.black.withOpacity(0.5),
                   Colors.transparent,
                 ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
+                begin: Alignment.bottomCenter,
+                end: Alignment.topCenter,
               ),
             ),
             child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  AnimatedText(),
-                  SizedBox(height: 50),
+                  const Center(
+                    child: Text(
+                      'Search For an Outfit by Uploading',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  const Center(
+                    child: Text(
+                      'Photo or Taking Image',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 50),
                   SizedBox(
-                    width: 200,
+                    width: 300,
                     height: 60,
                     child: ElevatedButton(
                       onPressed: () => _pickImage(ImageSource.gallery),
@@ -104,7 +182,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   SizedBox(height: 16),
                   SizedBox(
-                    width: 200,
+                    width: 300,
                     height: 60,
                     child: ElevatedButton(
                       onPressed: () => _pickImage(ImageSource.camera),
@@ -113,20 +191,52 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   SizedBox(height: 32),
                   SizedBox(
-                    width: 200,
+                    width: 300,
                     height: 60,
                     child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.transparent,
+                        side: BorderSide(
+                          width: 2.0,
+                          color: Colors.white,
+                        ),
+                      ),
                       onPressed: _navigateToResultPage,
-                      child: Text('Submit Image'),
+                      child: Text(
+                        'Submit Image',
+                        style: TextStyle(color: Colors.white),
+                      ),
                     ),
                   ),
                   SizedBox(height: 32),
-                  
                 ],
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CropImageScreen extends StatefulWidget {
+  final File image;
+
+  CropImageScreen({required this.image});
+
+  @override
+  _CropImageScreenState createState() => _CropImageScreenState();
+}
+
+class _CropImageScreenState extends State<CropImageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Crop Image'),
+      ),
+      body: Container(
+        child: Image.file(widget.image),
       ),
     );
   }
